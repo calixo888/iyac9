899c2a477c0db21b450fdd0574d0d9f31e88bd34
Calix Huang <calix.huang1@gmail.com>
2022-09-22 19:28:04 -0700

    setup main page structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <div className="h-screen bg-blue-100 justify-center items-center flex">
+        <h1 className="text-xl font-semibold">Header</h1>
+      </div>
+
+      <div className="h-screen bg-green-100 justify-center items-center flex">
+        <h1 className="text-xl font-semibold">Grid of IY9 Profiles</h1>
+      </div>
+
+      <div className="h-screen bg-red-100 justify-center items-center flex">
+        <h1 className="text-xl font-semibold">Digitized Mural w/ tooltips</h1>
+      </div>
+
+      <div className="h-screen bg-yellow-100 justify-center items-center flex">
+        <h1 className="text-xl font-semibold">Mural Timelapse video embed</h1>
+      </div>
+    </>
   )
 }
