959dba37e551efaf954b96a6f360a9e77c6e8d4e
Calix Huang <calix.huang1@gmail.com>
2022-10-01 19:34:30 -0700

    setup general structure of image, zoom/pinch package, and corner ui items

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,49 @@
 import type { NextPage } from 'next';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export default function Home() {
+  const [screenWidth, setScreenWidth] = useState(1400);
+  const [screenHeight, setScreenHeight] = useState(800);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    }
+  }, []);
+
   return (
-    <div>
-      <div className="h-screen bg-blue-100 justify-center items-center flex">
-        <h1 className="text-xl font-semibold">Header</h1>
-      </div>
-
-      <div className="h-screen bg-green-100 justify-center items-center flex">
-        <h1 className="text-xl font-semibold">Grid of IY9 Profiles</h1>
-      </div>
-
-      <div className="h-screen bg-red-100 justify-center items-center flex">
-        <h1 className="text-xl font-semibold">Digitized Mural w/ tooltips</h1>
-      </div>
-
-      <div className="h-screen bg-yellow-100 justify-center items-center flex">
-        <h1 className="text-xl font-semibold">Mural Timelapse video embed</h1>
-      </div>
+    <div className="w-screen h-screen bg-blue-100">
+      <TransformWrapper
+        initialScale={1}
+        // initialPositionX={200}
+        // initialPositionY={100}
+      >
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <React.Fragment>
+            <div className="z-10 bg-white fixed left-0 top-0">
+              <p>IYA C9</p>
+            </div>
+            <div className="z-10 bg-white fixed right-0 top-0">
+              <p>mural, people</p>
+            </div>
+            <div className="z-10 bg-white fixed left-0 bottom-0">
+              <p>socials</p>
+            </div>
+            <div className="z-10 bg-white fixed right-0 bottom-0">
+              <button onClick={() => zoomIn()}>+</button>
+              <button onClick={() => zoomOut()}>-</button>
+              <button onClick={() => resetTransform()}>reset</button>
+            </div>
+            <TransformComponent>
+              <Image src="/sunset.png" alt="test" width={screenWidth} height={screenHeight} />
+              {/* <div>Example text</div> */}
+            </TransformComponent>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
     </div>
   )
 }
