92b8505b712009bdbf170fed3ab94dcfcb4afffe
Hank N <henrywarren04@yahoo.com>
2022-10-07 13:23:30 -0700

    Fixed dark mode text issue
    Few more dark mode issues should be fixed then we will be good

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -10,10 +10,10 @@ const HeaderNav = () => (
     <div className="z-10 fixed right-3 top-3">
       <div className="flex flex-row items-center">
         <Link href="/#mural">
-          <div className="desktop bg-white py-2 px-5 rounded-full border-gray-1 shadow-lg text-md ml-3">Mural</div>
+          <div className="desktop bg-white text-black py-2 px-5 rounded-full border-gray-1 shadow-lg text-md ml-3">Mural</div>
         </Link>
         <Link href="/#people">
-          <div className="bg-white py-2 px-5 rounded-full border-gray-1 shadow-lg text-md ml-3">People</div>
+          <div className="bg-white py-2 px-5 text-black rounded-full border-gray-1 shadow-lg text-md ml-3">People</div>
         </Link>
       </div>
     </div>
