05dded329e1b75b11b0c27cd8249bf63e75c7668
Calix Huang <calix.huang1@gmail.com>
2022-10-06 23:07:47 -0700

    fixed safari stuff

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -4,13 +4,13 @@ const HeaderNav = () => (
   <>
     <div className="z-10 fixed left-3 top-3">
       <Link href="/#">
-        <div className="bg-white py-2 px-5 rounded-full border-gray-1 shadow-lg text-lg">☁️ IYA<strong>C9</strong></div>
+        <div className="bg-white text-black py-2 px-5 rounded-full border-gray-1 shadow-lg text-lg">☁️ IYA<strong>C9</strong></div>
       </Link>
     </div>
     <div className="z-10 fixed right-3 top-3">
       <div className="flex flex-row items-center">
         <Link href="/#mural">
-          <div className="bg-white py-2 px-5 rounded-full border-gray-1 shadow-lg text-md ml-3">Mural</div>
+          <div className="desktop bg-white py-2 px-5 rounded-full border-gray-1 shadow-lg text-md ml-3">Mural</div>
         </Link>
         <Link href="/#people">
           <div className="bg-white py-2 px-5 rounded-full border-gray-1 shadow-lg text-md ml-3">People</div>
